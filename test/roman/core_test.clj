166f2a8449c62de21d34c15e4da5bc824b56f74d
Andrew Whitehouse <andrew-github@whitehouse.org.uk>
2014-12-17 19:52:00 +0000

    Add case for I+I=II.

--- test/roman/core_test.clj
+++ test/roman/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [roman.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest simple-addition
+  (is (= "II" (add "I" "I"))))
+
+(run-tests 'roman.core-test)
